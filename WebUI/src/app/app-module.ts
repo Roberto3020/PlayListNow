509508bb5bd66988ca926192c0aad38456da6bb5
Roberto Carlos Orcasita Suarez <rorcasita@unicesar.edu.co>
2025-09-02 23:05:19 -0500

    Ajuste aplicativo angular

--- WebUI/src/app/app-module.ts
+++ WebUI/src/app/app-module.ts
@@ -1,16 +1,29 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SongListComponent } from './song-list/song-list.component';
+import { SongFormComponent } from './song-form/song-form.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     App
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+  AppRoutingModule,
+  SongFormComponent,
+  SongListComponent,
+  WelcomeComponent,
+  HttpClientModule,
+  BrowserAnimationsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- WebUI/src/app/app-routing-module.ts
+++ WebUI/src/app/app-routing-module.ts
@@ -1,7 +1,17 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SongListComponent } from './song-list/song-list.component';
+import { SongFormComponent } from './song-form/song-form.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'songs', component: SongListComponent },
+  { path: 'songs/add', component: SongFormComponent },
+  { path: 'songs/edit/:id', component: SongFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
