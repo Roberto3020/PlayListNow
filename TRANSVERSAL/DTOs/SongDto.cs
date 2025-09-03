2be1f9e7773cc6dcdadd3a37333dcd01a6e70f60
Roberto Carlos Orcasita Suarez <rorcasita@unicesar.edu.co>
2025-09-02 23:03:18 -0500

    Ajuste dto id opcional

--- TRANSVERSAL/DTOs/SongDto.cs
+++ TRANSVERSAL/DTOs/SongDto.cs
@@ -2,7 +2,7 @@ namespace TRANSVERSAL.DTOs
 {
     public class SongDto
     {
-        public int Id { get; set; }
+        public int? Id { get; set; } = 0;
         public string Title { get; set; } = string.Empty;
         public string Artist { get; set; } = string.Empty;
         public int Duration { get; set; }
